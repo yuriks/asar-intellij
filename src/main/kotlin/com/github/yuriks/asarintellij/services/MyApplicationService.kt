a866d5a4760be414a9d7a83eaf7ac3ddcdd5c103
GitHub Action <action@github.com>
2023-01-13 18:36:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/yuriks/asarintellij/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/yuriks/asarintellij/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.yuriks.asarintellij.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.yuriks.asarintellij.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/yuriks/asarintellij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/yuriks/asarintellij/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.yuriks.asarintellij.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.yuriks.asarintellij.MyBundle
 
 class MyProjectService(project: Project) {
 
